ca88ff270b6eb4418fb52bc944d1a7faae06ef82
mbret <bret.maxime@gmail.com>
2022-06-05 19:06:04 +0200

    misc

--- packages/imhentai-back/src/index.test.ts
+++ packages/imhentai-back/src/index.test.ts
@@ -12,7 +12,10 @@ it(`should retrieve metadata`, async () => {
     modifiedAt: ``,
     resourceId: dataSourceHelpers.generateResourceId(UNIQUE_RESOURCE_IDENTIFIER, `774006`),
     rx_model: `link`,
-    type: TYPE
+    type: TYPE,
+    rxdbMeta: {
+      lwt: new Date().getTime()
+    }
   }
 
   console.log(await plugin.getMetadata(link))
